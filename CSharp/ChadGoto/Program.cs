56d6a5e20307c0727b0b32481a9c972cada7a39e
Vitor Luiz Rubio <vitorrubio@gmail.com>
2023-02-08 12:29:02 -0300

    chad goto now becomes even more chadder

--- CSharp/ChadGoto/Program.cs
+++ CSharp/ChadGoto/Program.cs
@@ -13,20 +13,24 @@ void VirginLinq()
 
 void ChadGoTo()
 {
-    int[] values = { 5, 10, 13, 20, 25, 39, 50 };
-
-    int sum = 0;
-    int position = 0;
-
-starthere: //i'll do all the job again and again counting to potatoes
-
-    sum += values[position++];
-    if(position < values.Length)
+    unsafe
     {
-        goto starthere;
+        ReadOnlySpan<int> values = stackalloc[]{ 5, 10, 13, 20, 25, 39, 50 };
+
+        int sum = 0;
+        int position = 0;
+
+        starthere: //i'll do all the job again and again counting to potatoes
+
+        sum += values[position++];
+        if (position < values.Length)
+        {
+            goto starthere;
+        }
+
+        Console.WriteLine(sum);
     }
 
-    Console.WriteLine(sum);
 }
 
 
